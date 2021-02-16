ce55c1fe22dc970acfd4343d56f7f86729fe4d85
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-16 17:03:27 +0500

    chapter 3: echo with heredoc

---
+++ Chapter3/3_8.php
@@ -0,0 +1,9 @@
+<?php
+	$author = "Brian W. Kernighan";
+
+	echo <<<_END
+	Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by defenition, not smart enough to debug it.
+
+	$author.
+_END
+?>
