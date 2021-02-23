478441aa223e6640e12980b0bc640245d45cf802
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-23 18:03:22 +0500

    lesson 4: literals and variables

---
+++ Chapter4/4_3.php
@@ -0,0 +1,10 @@
+<?php
+	$myname = "Brian";
+	$myage = 37;
+
+	echo "a: " . 73 . "<br>";
+	echo "b: " . "Hello" . "<br>";
+	echo "c: " . FALSE . "<br>";
+	echo "d: " . $myname . "<br>";
+	echo "e: " . $myage . "<br>";
+?>
