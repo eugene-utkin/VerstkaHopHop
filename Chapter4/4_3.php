16885871af06a62ac70703104b31be6c8ce58b0c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-24 14:15:45 +0500

    lesson 4: comments added to literals and variables

--- Chapter4/4_3.php
+++ Chapter4/4_3.php
@@ -2,9 +2,9 @@
 	$myname = "Brian";
 	$myage = 37;
 
-	echo "a: " . 73 . "<br>";
-	echo "b: " . "Hello" . "<br>";
-	echo "c: " . FALSE . "<br>";
-	echo "d: " . $myname . "<br>";
-	echo "e: " . $myage . "<br>";
+	echo "a: " . 73 . "<br>"; // Числовой литерал
+	echo "b: " . "Hello" . "<br>"; // Строковый литерал
+	echo "c: " . FALSE . "<br>"; // Литерал константы 
+	echo "d: " . $myname . "<br>"; // Строковая переменная
+	echo "e: " . $myage . "<br>"; // Числовая переменная
 ?>
