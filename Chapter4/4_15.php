5524671478d1303daedddfd7223869b18d37b802
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-02-25 15:24:27 +0500

    chapter 4: comparison examples

---
+++ Chapter4/4_15.php
@@ -0,0 +1,8 @@
+<?php
+	$a = 2; $b = 3;
+
+	if ($a > $b) echo "$a больше $b<br>";
+	if ($a < $b) echo "$a меньше $b<br>";
+	if ($a >= $b) echo "$a больше или равно $b<br>";
+	if ($a <= $b) echo "$a меньше или равно $b<br>";
+?>
