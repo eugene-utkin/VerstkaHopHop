3099ad49b8add8458120535198332d65bdd193f9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-02 14:54:32 +0500

    chapter 4: too many elseif example

---
+++ Chapter4/4_22.php
@@ -0,0 +1,8 @@
+<?php
+	if ($page == "Home") echo "Вы выбрали Home";
+	elseif ($page == "About") echo "Вы выбрали About";
+	elseif ($page == "News") echo "Вы выбрали News";
+	elseif ($page == "Login") echo "Вы выбрали Login";
+	elseif ($page == "Links") echo "Вы выбрали Links";
+	else echo "Нераспозанный выбор";
+?>
