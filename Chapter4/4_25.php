f4915310f4c58c2e69c6e8affff9c586e7d1de39
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-02 15:03:17 +0500

    chapter 4: switch with endswitch example

---
+++ Chapter4/4_25.php
@@ -0,0 +1,22 @@
+<?php
+	switch ($page):
+		case "Home":
+			echo "Вы выбрали Home";
+			break;
+		case "About":
+			echo "Вы выбрали About";
+			break;
+		case "News":
+			echo "Вы выбрали News";
+			break;
+		case "Login":
+			echo "Вы выбрали Home";
+			break;
+		case "Links":
+			echo "Вы выбрали Links";
+			break;
+		default:
+			echo "Нераспознанный выбор";
+			break;
+	endswitch;
+?>
