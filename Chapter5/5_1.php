fab7e5cad16b50ff3497793baac20bf5be517d65
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-10 16:26:59 +0500

    chapter5: built-in functions demo

---
+++ Chapter5/5_1.php
@@ -0,0 +1,5 @@
+<?php
+	echo strrev(" .dlrow olleH"); // Реверсирование строки
+	echo str_repeat("Hip ", 2); // Повторение строки
+	echo strtoupper("hooray!"); // Преобразование символов строки в верхний регистр
+?>
