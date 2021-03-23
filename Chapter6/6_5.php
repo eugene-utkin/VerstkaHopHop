8561b81db2089208599be24fdbecd41678d48f28
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-23 19:04:11 +0500

    chapter 6: adding elements with array

---
+++ Chapter6/6_5.php
@@ -0,0 +1,12 @@
+<?php
+	$p1 = array("Copier", "Inkjet", "Laser", "Photo");
+
+	echo "Элемент массива p1: " . $p1[2] . "<br>";
+
+	$p2 = array('copier' => "Copier & Multipurpose",
+				'inkjet' => "Inkjet Printer",
+				'laser' => "Laser Printer",
+				'photo' => "Photographic Paper");
+
+	echo "Элемент массива p2: " . $p2['inkjet'] . "<br>";
+?>
