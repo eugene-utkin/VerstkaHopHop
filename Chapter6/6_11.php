7733e88b4ab799d4e16ceacf22d5c37de6defb34
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-25 16:34:56 +0500

    chapter 6: chessboard as array example

---
+++ Chapter6/6_11.php
@@ -0,0 +1,24 @@
+<?php
+	$chessboard = array(
+		array('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'),
+		array('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'),
+		array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+		array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+		array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+		array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '),
+		array('P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'),
+		array('R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R')
+	);
+
+	echo "<pre>";
+
+	foreach ($chessboard as $row)
+	{
+		foreach ($row as $piece)
+			echo "$piece ";
+
+		echo "<br>";
+	}
+
+	echo "</pre>";
+?>
