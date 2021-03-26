ce5645268aeb6d0e05347026627c7cd5481b1977
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-03-26 17:17:15 +0500

    chapter 6: compact example

---
+++ Chapter6/6_14.php
@@ -0,0 +1,11 @@
+<?php
+	$fname = "Doctor";
+	$sname = "Who";
+	$planet = "Gallifrey";
+	$system = "Gridlock";
+	$constellation = "Kasterborous";
+
+	$contact = compact('fname', 'sname', 'planet', 'system', 'constellation');
+
+	print_r($contact);
+?>
