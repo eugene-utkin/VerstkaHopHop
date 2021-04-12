3c57ce157f3b7ef88f3cbe76fa897325f3058af8
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-04-12 16:12:42 +0500

    Chapter 7: comparison

--- Chapter7/upload.php
+++ Chapter7/upload.php
@@ -1,4 +1,19 @@
 <?php // upload.php
+	echo <<<_END
+		<html><head><title>PHP-форма для загрузки файлов на сервер</title></head><body><form method='post' action='upload.php' enctype='multipart/form-data'> Выберите файл: <input type='file' name='filename' size='10'><input type='submit' value='Загрузить'></form>
+	_END;
+
+	if ($_FILES)
+	{
+		$name = $_FILES['filename']['name'];
+		move_uploaded_file($_FILES['filename']['tmp_name'], $name);
+		echo "Загружаемое изображение '$name'<br><img src='$name'>";
+	}
+
+	echo "</body></html>";
+?>
+
+<?php // upload.php
   echo <<<_END
     <html><head><title>PHP Form Upload</title></head><body>
     <form method='post' action='upload.php' enctype='multipart/form-data'>
