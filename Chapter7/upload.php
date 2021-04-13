980654d0938bc670de348f1f98a79549d72fc64d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-04-13 15:02:02 +0500

    Chapter 7: _END located correctly

--- Chapter7/upload.php
+++ Chapter7/upload.php
@@ -1,30 +1,11 @@
 <?php // upload.php
 	echo <<<_END
-		<html><head><title>PHP-форма для загрузки файлов на сервер</title></head><body><form method='post' action='upload.php' enctype='multipart/form-data'> Выберите файл: <input type='file' name='filename' size='10'><input type='submit' value='Загрузить'></form>
-	_END;
+		<html><head><title>PHP-форма для загрузки файлов на сервер</title></head><body> <form method='post' action='upload.php' enctype='multipart/form-data'> Выберите файл: <input type='file' name='filename' size='10'> <input type='submit' value='Загрузить'> </form>
+_END;
 
 	if ($_FILES)
 	{
 		$name = $_FILES['filename']['name'];
-		move_uploaded_file($_FILES['filename']['tmp_name'], $name);
-		echo "Загружаемое изображение '$name'<br><img src='$name'>";
-	}
-
-	echo "</body></html>";
-?>
-
-<?php // upload.php
-  echo <<<_END
-    <html><head><title>PHP Form Upload</title></head><body>
-    <form method='post' action='upload.php' enctype='multipart/form-data'>
-    Select File: <input type='file' name='filename' size='10'>
-    <input type='submit' value='Upload'>
-    </form>
-_END;
-
-  if ($_FILES)
-  {
-    $name = $_FILES['filename']['name'];
 		move_uploaded_file($_FILES['filename']['tmp_name'], $name);
 		echo "Загружаемое изображение '$name'<br><img src='$name'>";
 	}
