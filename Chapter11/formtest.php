a2d3ffa9d5d9164ed7b686126ce793de8b2eb895
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-06-04 12:56:10 +0500

    Chapter 11: simple form example

---
+++ Chapter11/formtest.php
@@ -0,0 +1,16 @@
+<?php // formtest.php - просто обработчик формы на PHP
+ echo <<< END
+ 	<html>
+ 		<head>
+ 			<title>Form Test</title>
+ 		</head>
+ 		<body>
+ 			<form method="post" action="formtest.php">
+ 				Как вас зовут?
+ 				<input type="text" name="name">
+ 				<input type="submit">
+ 			</form>
+ 		</body>
+ 	</html>
+_END;
+?>
