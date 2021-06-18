d532c675f3ef7dcf191ff266dcf08bbc2039e310
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-06-18 13:23:58 +0500

    Chapter 12: php-authentication via MySQL example (90% done)

--- Chapter12/authenticate.php
+++ Chapter12/authenticate.php
@@ -24,8 +24,19 @@
 				Hi $row[0], you are logged in as '$row[2]'");
 			else die("Неверная комбинация имя пользователя - пароль");
 		}
-		else die("")
-
+		else die("Неверная комбинация имя пользователя - пароль");
+	}
+	else
+	{
+		header('WWW-Authenticate: Basic realm="Restricted Area"');
+		header('HTTP/1.0 401 Unauthorized');
+		die ("Пожалуйста, введите имя пользователя и пароль");
 	}
 
+	$connection->close();
+
+	function mysql_entities_fix_string($connection, $string)
+	{
+		return htmlentities(mysql_fix_string($connection, $string));
+	}
 ?>
