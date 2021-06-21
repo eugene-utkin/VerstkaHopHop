c176aa7318d4b026b83afede03a3723ce3edeb76
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-06-21 12:56:45 +0500

    Chapter 12: php-authentication via MySQL example (100%)

--- Chapter12/authenticate.php
+++ Chapter12/authenticate.php
@@ -39,4 +39,10 @@
 	{
 		return htmlentities(mysql_fix_string($connection, $string));
 	}
+
+	function mysql_fix_string($connection, $string)
+	{
+		if (get_magic_quotes_gpc()) $string = stripslashes($string);
+		return $connection->real_escape_string($string);
+	}
 ?>
