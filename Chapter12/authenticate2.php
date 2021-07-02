f2960451ad3d01fc62d106092723124fc8037502
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-07-02 10:58:10 +0500

    Chapter 12: Opening session after successful authorization (more modifications)

--- Chapter12/authenticate2.php
+++ Chapter12/authenticate2.php
@@ -10,29 +10,27 @@
 		header('HTTP/1.0 401 Unauthorized');
 		die ("Пожалуйста, введите имя пользователя и пароль");
 	}
-		$un_temp = mysql_entities_fix_string($connection, $_SERVER['PHP_AUTH_USER']);
-		$pw_temp = mysql_entities_fix_string($connection, $_SERVER['PHP_AUTH_PW']);
-		$query = "SELECT * FROM users WHERE username='$un_temp'";
-		$result = $connection->query($query);
-
-		if (!$result) die("Пользователь не найден");
-		elseif ($result->num_rows)
-		{
-			$row = $result->fetch_array(MYSQLI_NUM);
-			$result->close();
-			if (password_verify($pw_temp, $row[3]))
-			{
-				session_start();
-				$_SESSION['forename'] = $row[0];
-				$_SESSION['surname'] = $row[1];
-				echo htmlspecialchars("$row[0] $row[1] : Привет, $row[0], теперь вы зарегистрированы под именем '$row[2]'");
-				die ("<p><a href='continue.php'>Щелкните здесь для продолжения</a></p>");
-			}
-			else die("Неверная комбинация имя пользователя - пароль");
-		}
-		else die("Неверная комбинация имя пользователя - пароль");
+	$un_temp = mysql_entities_fix_string($connection, $_SERVER['PHP_AUTH_USER']);
+	$pw_temp = mysql_entities_fix_string($connection, $_SERVER['PHP_AUTH_PW']);
+	$query = "SELECT * FROM users WHERE username='$un_temp'";
+	if (!$result = $connection->query($query)) {
+		die($connection->error);
+	}
+	if (!$result->num_rows) {
+		die("Неверная комбинация имя пользователя - пароль");
+	}
+	$row = $result->fetch_array(MYSQLI_NUM);
+	$result->close();
+	if (password_verify($pw_temp, $row[3])) {
+		session_start();
+		$_SESSION['forename'] = $row[0];
+		$_SESSION['surname'] = $row[1];
+		echo "$row[0] $row[1] : Привет, $row[0], теперь вы зарегистрированы под именем '$row[2]'";
+		die ("<p><a href='continue.php'>Щелкните здесь для продолжения</a></p>");
+	}
 
 	$connection->close();
+	
 	function mysql_entities_fix_string($connection, $string)
 	{
 		return htmlentities(mysql_fix_string($connection, $string));
