32c36b2638f65b497a4d748b80e820f5d0b7e8b5
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-07-07 11:06:57 +0500

    Chapter 13: Hello World with JavaScript

---
+++ Chapter13/13_1.php
@@ -0,0 +1,11 @@
+<html>
+	<head><title>Hello World</title></head>
+	<body>
+		<script type="text/javascript">
+			document.write("Hello World")
+		</script>
+		<noscript>
+			Ваш браузер не поддерживает JavaScript, или его поддержка отключена
+		</noscript>
+	</body>
+</html>
