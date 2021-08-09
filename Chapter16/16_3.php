bd8b74b54fcb460c338e60f327e0b36b1fbbd879
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-08-09 10:53:08 +0500

    Chapter 16: adduser.php (PHP part done + JS part done + 90% second PHP part done)

--- Chapter16/16_3.php
+++ Chapter16/16_3.php
@@ -142,5 +142,50 @@
 _END;
 
 	// PHP-функции
+
+	function validate_forename($field)
+	{
+		return ($field == "") ? "Не введено имя<br>" : "";
+	}
+
+	function validate_surname($field)
+	{
+		return ($field == "") ? "Не введена фамилия<br>" : "";
+	}
+
+	function validate_username($field)
+	{
+		if ($field == "") return "Не введено имя пользователя<br>";
+		else if (strlen($field) < 5)
+			return "В имени пользователя должно быть не менее 5 символов<br>";
+		else if (preg_match("/[^a-zA-Z0-9_-]/", $field))
+			return "В имени пользователя допускаются только буквы, цифры, - и _<br>";
+		return "";
+	}
+
+	function validate_password($field)
+	{
+		if ($field == "") return "Не введен пароль<br>";
+		else if (strlen($field) < 6)
+			return "В пароле должно быть не менее 6 символов<br>";
+		else if (!preg_match("/[a-z]/", $field) || !preg_match("/[A-Z]/", $field) || !preg_grep("/[0-9]/", $field))
+			return "Пароль требует 1 символ из каждого набора a-z, A-Z и 0-9<br>";
+		return "";
+	}
 	
+	function validate_age($field)
+	{
+		if ($field == "") return "Не введен возраст<br>";
+		else if ($field < 18 || $field > 110)
+			return "Возраст должен быть между 18 и 110<br>";
+		return "";
+	}
+
+	function validate_email($field)
+	{
+		if ($field == "") return "Не введен адрес электронной почты<br>";
+		else if (!((strpos($field, ".") > 0) && (strpos($field, "@") > 0)) || preg_match("/[^a-zA-Z0-9.@_-]/", $field))
+			return "Электронный адрес имеет неверный формат<br>";
+		return "";
+	}
 ?>
