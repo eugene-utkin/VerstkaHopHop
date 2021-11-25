c942daac603c55c7c31593fc39b6ca308e39f11f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-11-25 20:45:59 +0500

    Chapter 27: jQuery 2.2.4 added to lesson folder

---
+++ Chapter27/27_2.php
@@ -0,0 +1,18 @@
+<?php //
+  session_start();
+
+echo <<<_INIT
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='jquery.mobile-1.4.5.min.css'>
+    <link rel='stylesheet' href='styles.css'>
+    <script src='javascript.js'></script>
+    <script src='jquery-2.2.4.min.js'></script>
+    <script src='jquery.mobile-1.4.5.min.js'></script>
+
+_INIT;
+
+  require_once
