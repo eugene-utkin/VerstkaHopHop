eb46890ff54e8d9fa91541fcea552fe9db3688b6
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-11-27 16:18:09 +0500

    Chapter 27: typos fixed

--- Chapter27/27_2.php
+++ Chapter27/27_2.php
@@ -8,7 +8,7 @@ echo <<<_INIT
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' href='jquery.mobile-1.4.5.min.css'>
-    <link rel='stylesheet' href='styles.css'>
+    <link rel='stylesheet' href='styles.css' type='text/css'>
     <script src='javascript.js'></script>
     <script src='jquery-2.2.4.min.js'></script>
     <script src='jquery.mobile-1.4.5.min.js'></script>
@@ -43,26 +43,26 @@ _MAIN;
   if ($loggedin)
   {
 echo <<<_LOGGEDIN
-      <div class='center'>
-        <a data-role='button' data-inline='true' data-icon='home' data-transition='slide' href='members.php?view=$user'>Home</a>
-        <a data-role='button' data-inline='true' data-transition='slide' href='members.php'>Members</a>
-        <a data-role='button' data-inline='true' data-transition='slide' href='friends.php'>Friends</a>
-        <a data-role='button' data-inline='true' data-transition='slide' href='messages.php'>Messages</a>
-        <a data-role='button' data-inline='true' data-transition='slide' href='profile.php'>Edit Profile</a>
-        <a data-role='button' data-inline='true' data-transition='slide' href='logout.php'>Log out</a>
-      </div>
+        <div class='center'>
+          <a data-role='button' data-inline='true' data-icon='home' data-transition='slide' href='members.php?view=$user'>Home</a>
+          <a data-role='button' data-inline='true' data-transition='slide' href='members.php'>Members</a>
+          <a data-role='button' data-inline='true' data-transition='slide' href='friends.php'>Friends</a>
+          <a data-role='button' data-inline='true' data-transition='slide' href='messages.php'>Messages</a>
+          <a data-role='button' data-inline='true' data-transition='slide' href='profile.php'>Edit Profile</a>
+          <a data-role='button' data-inline='true' data-transition='slide' href='logout.php'>Log out</a>
+        </div>
 
 _LOGGEDIN;
   }
   else
   {
 echo <<<_GUEST
-      <div class='center'>
-        <a data-role='button' data-inline='true' data-icon='home' data-transition='slide' href='index.php'>Home</a>
-        <a data-role='button' data-inline='true' data-icon='plus' data-transition='slide' href='signup.php'>Sign Up</a>
-        <a data-role='button' data-inline='true' data-icon='check' data-transition='slide' href='login.php'>Log In</a>
-      </div>
-      <p class='info'>(You must be logged in to use this app)</p>
+        <div class='center'>
+          <a data-role='button' data-inline='true' data-icon='home' data-transition='slide' href='index.php'>Home</a>
+          <a data-role='button' data-inline='true' data-icon='plus' data-transition='slide' href='signup.php'>Sign Up</a>
+          <a data-role='button' data-inline='true' data-icon='check' data-transition='slide' href='login.php'>Log In</a>
+        </div>
+        <p class='info'>(You must be logged in to use this app)</p>
 
 _GUEST;
   }
