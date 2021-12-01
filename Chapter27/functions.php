1419fb3d5363cb2d37667236023c13913cae46f2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-01 17:38:45 +0500

    Chapter 27: typo fixed

--- Chapter27/functions.php
+++ Chapter27/functions.php
@@ -24,7 +24,7 @@
   function destroySession()
   {
     $_SESSION=array();
-    if (sessin_id() != "" isset($_COOKIE[session_name()]))
+    if (sessin_id() != "" || isset($_COOKIE[session_name()]))
       setcookie(session_name(), '', time()-2592000, '/');
 
     session_destroy();
