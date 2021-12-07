094001ede1df3eb4d86a2f919241fcfe720ccb8c
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-07 17:17:31 +0500

    Chapter 27: typo fixed

--- Chapter27/27_7.php
+++ Chapter27/27_7.php
@@ -11,7 +11,8 @@
       $error = 'Not all fields were entered';
     else
     {
-      $result = queryMySQL("SELECT user,pass FROM members WHERE user='$user' AND pass='$pass'");
+      $result = queryMySQL("SELECT user,pass FROM members
+        WHERE user='$user' AND pass='$pass'");
 
       if ($result->num_rows == 0)
       {
@@ -27,28 +28,29 @@
   }
 
 echo <<<_END
-    <form method='post' action='login.php'>
-      <div data-role='fieldcontain'>
-        <label></label>
-        <span class='error'>$error</span>
-      </div>
-      <div data-role='fieldcontain'>
-        <label></label>
-        Please enter your details to log in
-      </div>
-      <div data-role='fieldcontain'>
-        <label>Username</label>
-        <input type='text' maxlength='16' name='user' value='$user'>
-      </div>
-      <div data-role='fieldcontain'>
-        <label>Password</label>
-        <input type='password' maxlength='16' name='pass' value='$pass'>
-      </div>
-      <div data-role='fieldcontain'>
-        <label></label>
-        <input data-transition='slide' type='submit' value='Login'>
-      </div>
-    </form>
+      <form method='post' action='login.php'>
+        <div data-role='fieldcontain'>
+          <label></label>
+          <span class='error'>$error</span>
+        </div>
+        <div data-role='fieldcontain'>
+          <label></label>
+          Please enter your details to log in
+        </div>
+        <div data-role='fieldcontain'>
+          <label>Username</label>
+          <input type='text' maxlength='16' name='user' value='$user'>
+        </div>
+        <div data-role='fieldcontain'>
+          <label>Password</label>
+          <input type='password' maxlength='16' name='pass' value='$pass'>
+        </div>
+        <div data-role='fieldcontain'>
+          <label></label>
+          <input data-transition='slide' type='submit' value='Login'>
+        </div>
+      </form>
+    </div>
   </body>
 </html>
 _END;
