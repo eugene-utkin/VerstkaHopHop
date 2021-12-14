337f42077bfc38c460a647c41400299c991590c9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-14 15:26:43 +0500

    Chapter 27: yet another typo fixed

--- Chapter27/27_9.php
+++ Chapter27/27_9.php
@@ -32,7 +32,7 @@
   }
 
   $result = queryMysql("SELECT user FROM members ORDER BY user");
-  $num = $result->num_rows;
+  $num    = $result->num_rows;
 
   echo "<h3>Other Members</h3><ul>";
 
@@ -47,18 +47,19 @@
 
     $result1 = queryMysql("SELECT * FROM friends WHERE
       user='" . $row['user'] . "' AND friend='$user'");
-    $t1 = $result1->num_rows;
+    $t1      = $result1->num_rows;
     $result1 = queryMysql("SELECT * FROM friends WHERE
       user='$user' AND friend='" . $row['user'] . "'");
-    $t2 = $result1->num_rows;
+    $t2      = $result1->num_rows;
 
     if (($t1 + $t2) > 1) echo " &harr; is a mutual friend";
-    elseif ($t1) echo " $larr; you are following";
-    elseif ($t2) { echo " $rarr; is following you"; $follow = "recip"; }
+    elseif ($t1)         echo " &larr; you are following";
+    elseif ($t2)       { echo " $rarr; is following you";
+                         $follow = "recip"; }
 
     if (!$t1) echo " [<a data-transition='slide'
       href='members.php?add=" . $row['user'] . "'>$follow</a>]";
-    else echo " [<a data-transition='slide' href='members.php?remove=" . $row['user'] . "'>drop</a>]";
+    else      echo " [<a data-transition='slide' href='members.php?remove=" . $row['user'] . "'>drop</a>]";
   }
 ?>
     </ul></div>
