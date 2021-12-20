93b347b5db38129369c5fb2d9c412a018b09abb9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-12-20 12:19:43 +0500

    Chapter 27: messages php file created

--- Chapter27/27_11.php
+++ Chapter27/27_11.php
@@ -39,4 +39,55 @@ if ($view != "")
         <input type='radio' name='pm' id='private' value='1'>
         <label for="private">Private</label>
       </fieldset>
-}
+      <textarea name='text'></textarea>
+      <input data-transition='slide' type='submit' value='Post Message'>
+    </form><br>
+_END;
+
+    date_default_timezone_set('UTC');
+
+    if (isset($_GET['erase']))
+    {
+      $erase = sanitizeString($_GET['erase']);
+      queryMysql("DELETE FROM messages WHERE id=$erase AND recip='$user'");
+    }
+
+    $query = "SELECT * FROM messages WHERE recip='$view' ORDER BY time DESC";
+    $result = queryMysql($query);
+    $num = $result->num_rows;
+
+    for ($j = 0 ; $j < $num ; ++$j)
+    {
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+
+      if ($row['pm'] == 0 || $row['auth'] == $user ||
+          $row['recip'] == $user)
+      {
+        echo date('M jS \'y g:ia:', $row['time']);
+        echo " <a href='messages.php?view=" . $row['auth'] .
+             "'>" . $row['auth']. "</a> ";
+
+        if ($row['pm'] == 0)
+          echo "wrote: &quot;" . $row['message'] . "&quot; ";
+        else
+          echo "whispered: <span class='whisper'>&quot;" .
+            $row['message']. "&quot;</span> ";
+
+        if ($row['recip'] == $user)
+          echo "[<a href='messages.php?view=$view" .
+               "&erase=" . $row['id'] . "'>erase</a>]";
+
+        echo "<br>";
+      }
+    }
+  }
+
+  if (!$num)
+    echo "<br><span class='info'>No messages yet</span><br><br>";
+
+  echo "<br><a data-role='button'
+        href='messages.php?view=$view'>Refresh messages</a>";
+?>
+    </div><br>
+  </body>
+</html>
